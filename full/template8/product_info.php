66f18359e201c64894082e7af89db2c2f265802e
Kavita Aggarwal <tech1@outdoorbusinessnetwork.com>
2016-11-07 07:16:17 -0500

    template8

---
+++ full/template8/product_info.php
@@ -0,0 +1,64 @@
+<div id="productinfo">    
+    <div id="leftpart">        
+        <h1>DISPLAY_PRODUCT_NAME</h1>        
+        <div id="proimg">            
+            <div id="mainImg">                
+                DISPLAY_PRODUCT_IMAGE              
+            </div>            
+            <div id="moreImg">                
+                DISPLAY_PRODUCT_EXTRA_IMAGE            
+            </div>        
+        </div>           
+        <div id="pro_info">            
+            <p class="itembg">Product Description</p>            
+            <div class="prodetail">DISPLAY_PRODUCT_DESCRIPTION</div>        
+        </div> 
+        <div id="pro_selec">            
+            <p class="itembg">Product Selections</p>            
+            <div class="prodetail">DISPLAY_CHILD_PRODUCTS</div>        
+        </div>       
+        <div id="pro_spec">            
+            <p class="itembg">Product Specifications</p>            
+            <div class="prodetail">DISPLAY_PRODUCT_SPECIFICATIONS</div>        
+        </div>        
+        <div id="pro_review">            
+            <p class="itembg">Product Reviews</p>            
+            <div class="prodetail">DISPLAY_PRODUCT_RATINGS | DISPLAY_PRODUCT_WRITE_RATINGS</div>            
+            <div class="prodetail">DISPLAY_PRODUCT_REVIEWS</div>        
+        </div>        
+        <div id="pro_package">            
+            <p class="itembg">Product Package</p>            
+            <div class="prodetail">DISPLAY_PACKAGE</div>        
+        </div>    </div>    <div id="rightpart">        
+            <div id="cartdiv">            
+                <p>DISPLAY_PRODUCT_MANUFACTURER</p>            
+                <p id="productItem12">DISPLAY_PRODUCT_MODEL</p>            
+                <p id="productPrice12">DISPLAY_CHILD_PRODUCT_PRICE</p>            
+                <div>                
+                    <table>DISPLAY_AVAILABILITY_N_PRICE</table>            
+                </div>            
+                <div>                
+                    <p>DISPLAY_PRODUCT_OPTIONS_TITLE</p>                
+                    DISPLAY_PRODUCT_ATTRIBUTES            
+                </div>            
+                <div>DISPLAY_PRODUCT_QUANTITY</div>            
+                <div>DISPLAY_PRODUCT_DISCLAIMER</div>            
+                <div>DISPLAY_PRODUCT_ADD_TO_CART <input type="submit" value="add to cart" class="addtocart_btn" id="disc_cart_btn"></div>            
+                <div>DISPLAY_PRODUCT_ADD_TO_WISHLIST<input type="submit" class="skubutton" name="wishlist_x" value="Add to Wishlist"></div>            
+                <div id="sharelink">DISPLAY_PRODUCT_SHARE_LINK</div>        
+            </div>
+            <div>DISPLAY_PRODUCT_RELATED_ITEMS</div>    
+        </div>            
+</div>  
+<script type="text/javascript">  
+var is_disclaimer = DISCLAIMER_ADD_TO_CART;
+    oPt(document).ready(function() {        
+        oPt('td.childButton').each(function() {            
+			oPt(this).children().eq(1).replaceWith('<input type="submit" value="add to cart" class="addtocart_btn">');        
+        });  
+		
+		if(is_disclaimer == 1){
+			oPt('#disc_cart_btn').attr('onclick','return disclaimer_onclick();');
+		}  
+    });
+</script>
