249c8a69301cd065600fc5c8d04ff036c5d7eded
Kavita Aggarwal <tech1@outdoorbusinessnetwork.com>
2016-12-07 06:14:57 -0500

    template 16 files

---
+++ full/template16/product_listing_large.php
@@ -0,0 +1,43 @@
+<!--<a href="#">
+<table cellpadding="0" border="0" cellspacing="0" width="100%" style="margin: 0 0 8px 0;" class="categoryListBoxContents subproduct_name" style="border:5px green solid;">
+<tr height="150px">
+  <td width="25%">DISPLAY_PRODUCT_IMAGE</td>
+</tr>
+<tr>
+  <td   width="25%" class="smallText">DISPLAY_PRODUCT_NAME<br />
+    <br /></td>
+</tr>
+<tr valign="top">
+  <td  width="25%"  class="smallText">DISPLAY_PRODUCT_STOCK<br />
+    <br /></td>
+</tr>
+<tr valign="top">
+  <td  width="25%" class="smallText">DISPLAY_PRODUCT_PRICE<br />
+    DISPLAY_PRODUCT_COMPARE<br /></td>
+</tr>
+
+<div class="Quik_view" style="border:1px red solid"> <span class="Quik_view_btn ">View</span> </div>
+</table>
+</a> -->
+<div class="col-md-12 product_hover new_product_listingBox"> 
+  
+  <div class="panel_default_featured_product">
+  
+    <div class="panel-body-category-image_new">DISPLAY_PRODUCT_IMAGE</div>
+    <div style="clear:both"></div>
+    <div class="panel-heading">
+      <div class="product_name new_product_name">
+        <p>DISPLAY_PRODUCT_NAME</p> 
+      </div>
+      <div class="product_name">
+        <p>DISPLAY_PRODUCT_STOCK</p> 
+      </div>
+      <div class="product_price_new_compare">
+        <p>DISPLAY_PRODUCT_PRICE</p>
+        <p>DISPLAY_PRODUCT_COMPARE</p>
+      </div>
+    </div>
+    
+   </div>
+ 
+</div>
